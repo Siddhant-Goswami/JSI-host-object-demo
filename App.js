7243b6cb30ce28195fe1007b5e6efbf3ddc38fc9
Siddhant Goswami <goswami.siddh@gmail.com>
2019-10-11 18:16:46 +0530

    Added nativeCall method and message.

--- App.js
+++ App.js
@@ -6,25 +6,35 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 type Props = {};
 export default class App extends Component<Props> {
+  async nativeCall() {
+    return global.nativeTest.runTest();
+  }
   render() {
-    console.warn(global.nativeTest.runTest());
+    let t1 = Date.now();
+    let t2;
+    this.nativeCall()
+      .then(() => (t2 = Date.now()))
+      .then(() => console.warn("TIME", t2 - t1))
+      .catch(err => console.log("Catch", err));
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>
+          You can see the timestamp difference in making a native call through
+          JSI in yellow warning box.
+        </Text>
       </View>
     );
   }
@@ -33,18 +43,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
